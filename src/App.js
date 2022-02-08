e42873e714408a86f762d9052085077cb36755b7
Sathishkumar <sathish04vsk@gmail.com>
2022-02-08 08:02:56 +0530

    trailer added in data

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ export default function App() {
           <Route path="/" exact><h1 className="home">sample application</h1></Route>
           <Route path="/flims"><Redirect to="/Movies/"></Redirect></Route>
           <Route path="/Movies" component={Movies} exact></Route>
+          <Route path="/Movies/:id" exact></Route>
           <Route path="/Color_game" exact component={Addcolor}></Route>
           <Route path="/tic_tac_tce" exact component={TicTacToe}></Route>
           <Route path="/about" exact></Route>
